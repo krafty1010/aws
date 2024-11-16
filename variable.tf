1dd88befc837749973338aad88e9731caa8f324e
Paul <paul@MBA.local>
2024-11-16 15:16:35 +0000

    vpc tested

---
+++ variable.tf
@@ -0,0 +1,31 @@
+#project
+
+variable "project" {
+  type    = string
+  default = "krafty"
+}
+
+#VPC
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "subnet_cidrs" {
+  description = "CIDR blocks for the subnets"
+  type        = list(string)
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+
+
