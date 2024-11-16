93adf52785d8efb4512815b1bd259acc035c0225
Paul <paul@MBA.local>
2024-11-16 15:27:37 +0000

    private subnets only

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -12,9 +12,9 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "private" {
   count = length(var.private_subnet_cidrs)
 
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = var.private_subnet_cidrs[count.index]
-  availability_zone       = var.availability_zones[count.index]
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
   map_public_ip_on_launch = false
 
   tags = {
@@ -22,44 +22,12 @@ resource "aws_subnet" "private" {
   }
 }
 
-# Create an Internet Gateway (optional, if private subnets need NAT access)
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "${var.project}-private-subnet"
-  }
-}
-
-# Create a NAT Gateway (optional, for outbound internet access from private subnets)
-resource "aws_eip" "nat" {
-  count = 1
-  vpc   = true
-  tags = {
-    Name = "${var.project}-nat-eip"
-  }
-}
-
-resource "aws_nat_gateway" "nat" {
-  allocation_id = aws_eip.nat[0].id
-  subnet_id     = aws_subnet.private[0].id # Assuming the first subnet is a public subnet
-  tags = {
-    Name = "${var.project}-nat-gw"
-  }
-}
-
-# Route table for private subnets
+# (Optional) Route table for private subnets (no NAT Gateway or IGW)
 resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
   tags = {
     Name = "${var.project}-private-routetable"
   }
-}
-
-# Route private subnets to NAT Gateway
-resource "aws_route" "nat_route" {
-  route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat.id
 }
 
 # Associate private subnets with the route table
